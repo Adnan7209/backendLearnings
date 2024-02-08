0207e7228f3835da28e26e2c29fde958f2fc7171
Adnan7209 <mdadnan28713@gmail.com>
2024-02-08 18:00:03 +0530

    tut01 ended

---
+++ tut01/math.js
@@ -0,0 +1,13 @@
+/* const add = (a,b) => a+b; 
+const sub = (a,b) => a-b; 
+const multi = (a,b) => a*b; 
+const div = (a,b) => a/b;
+
+module.exports = {add,sub,multi,div}
+ */
+
+exports.add = (a,b) => a+b; 
+exports.sub = (a,b) => a-b; 
+exports.multi = (a,b) => a*b; 
+exports.div = (a,b) => a/b;
+
--- tut01/server.js
+++ tut01/server.js
@@ -3,8 +3,10 @@ console.log("hello");
 //global object
 // console.log(global);
 
-const os = require('os');
+// comment1
+/* const os = require('os');
 const path = require('path');
+
 
 console.log(os.type());
 console.log(os.version());
@@ -19,4 +21,15 @@ console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 
 
-console.log(path.parse(__filename));
+console.log(path.parse(__filename)); */
+
+
+
+/* const math = require('./math');
+console.log(math.add(2,5)); */
+
+const {add, sub,multi,div} = require('./math');
+console.log(add(2,5));
+console.log(sub(2,10));
+console.log(multi(2,10));
+console.log(div(10,2));
